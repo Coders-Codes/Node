c3eb2aa73b92682f66c5a7f1d00daf80deb5fe37
Coders_Codes <khushichhoudhary890@gmail.com>
2023-08-23 16:20:55 +0530

    Task2 Program

--- code1.js
+++ code1.js
@@ -1,1 +1,19 @@
-console.log('Hello World!');
+
+ //Arrow Function
+const mulitply = (a,b) => a*b;
+console.log(mulitply(75,15)); 
+
+//Object Creation
+const student = {
+    name:'Khushi',
+    age:23,
+    greet () {
+        console.log('Hi, I am ' + this.name)
+    }
+    // greet :function() {
+    //     console.log('Hi, I am ' + this.name)
+    // }
+};
+console.log(student);
+student.greet();
+
