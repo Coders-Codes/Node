8eb51347807181851062ba96c5a4a4556ae5de4f
Coders_Codes <khushichhoudhary890@gmail.com>
2023-08-24 16:38:45 +0530

    Task7 Program

--- code1.js
+++ code1.js
@@ -1,5 +1,16 @@
-const http = require ('http')
-const server = http.createServer((req,res)=>{
-  console.log('Khushi Choudhary')
-}) 
-server.listen(4000);
+// const http = require ('http');
+// const server = http.createServer((req, res) => {
+//   res.write ("<h1>Welcome to my Node Js project</h1>");
+//   res.end();
+// }).listen(7500)
+
+const http = require ('http');
+const server = http.createServer((req, res) => {
+  res.setHeader('Content-Type','text/html')
+  res.write('<html>');
+  res.write('<head><title>My first Page</title></head>');
+  res.write ("<body><h1>Welcome to my Node Js project</h1></body>");
+  res.write('</html>')
+  res.end();
+}).listen(7500)
+
