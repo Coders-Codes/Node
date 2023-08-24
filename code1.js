dd6f67282602ca78bffabbd90aaedb3b20016884
Coders_Codes <khushichhoudhary890@gmail.com>
2023-08-24 12:34:05 +0530

    Task6 Program

--- code1.js
+++ code1.js
@@ -1,28 +1,5 @@
-async function values (){
-  try{
-    console.log("a");
-    console.log("b");
-    
-    const c = await new Promise((res, rej) => {
-        setTimeout(() => {
-          console.log("c");
-          res();
-        }, 3000);
-      });
-    
-    
-    const d = await new Promise((res, rej)=>{
-        setTimeout(() => {
-          console.log("d");
-          res();
-        },0)
-      });
-    
-    
-    console.log("e");
-  }catch (err) {
-    console.log(err);
-    }
-}
-values();
-
+const http = require ('http')
+const server = http.createServer((req,res)=>{
+  console.log('Khushi Choudhary')
+}) 
+server.listen(4000);
