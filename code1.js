b903fdef3f035165b9914bdc25f0c3a98da685c6
Coders_Codes <khushichhoudhary890@gmail.com>
2023-09-06 11:33:31 +0530

    Task1 Express Program

--- code1.js
+++ code1.js
@@ -1,6 +1,18 @@
-const http = require('http');
-const routes = require('./routes.js')
 
-const server = http.createServer(routes);
-server.listen(7500);
+const express = require('express') // importing express
+const app =express(); // creating express application b running express as a function
+//app here is a valid request handler so you so you can pass app to create server
+
+app.use((req, res, next) => {
+    console.log('In the Middleware');
+    next(); // allows the request to continue to the next middleware
+});
+
+app.use((req, res, next) => {
+    console.log('In another Middleware');
+    res.send('<h1>Hello from Express!</h1>')
+})
+
+
+app.listen(7500);
 
